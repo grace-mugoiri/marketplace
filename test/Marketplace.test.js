aaf238ae93d7e49e5712ad0dc47eddc61177e0c4
Grace Mugoiri <gracemugoiri@gmail.com>
2021-07-29 17:59:55 +0300

    add event product test

--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,28 +1,51 @@
-const Marketplace = artifacts.require('./Marketplace.sol')
-// require('chai')
-//     .use(require('chai-as-promised'))
-//     .should()
+const { assert } = require('chai');
 
-contract('Marketplace', (accounts) => {
-    let marketplace
-    
+const Marketplace = artifacts.require('./Marketplace.sol');
+require('chai')
+  .use(require('chai-as-promised'))
+  .should();
+
+contract('Marketplace', ([deployer, seller, buyer]) => {
+  let marketplace;
+
+  before(async () => {
+    marketplace = await Marketplace.deployed();
+  });
+
+  describe('deployment', async () => {
+    it('deploys successfully', async () => {
+      const address = await marketplace.address;
+      assert.notEqual(address, 0x0);
+      assert.notEqual(address, '');
+      assert.notEqual(address, null);
+      assert.notEqual(address, undefined);
+    });
+
+    it('has a name', async () => {
+      const name = await marketplace.name();
+      assert.equal(name, 'Grace Marketplace');
+    });
+  });
+
+  describe('products', async () => {
+    let result; 
+    let productCount;
     before(async () => {
-        marketplace = await Marketplace.deployed()
-    })
+      result = await marketplace.createProduct('Think Deeper', web3.utils.toWei('1', 'Ether'), { from: seller });
+      productCount = await marketplace.productCount();
+    });
 
-    describe('deployment', async () => {
-        it('deploys successfully', async () => {
-            const address = await marketplace.address
-            assert.notEqual(address, 0x0)
-            assert.notEqual(address, '')
-            assert.notEqual(address, null)
-            assert.notEqual(address, undefined)
-        })
+     it('creates product', async () => {
+         assert.equal(productCount, 1)
+         const event = result.logs[0].args
+         assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
+         assert.equal(event.name, 'Think Deeper', 'name is correct')
+         assert.equal(event.price, '1000000000000000000', 'price is correct')
+         assert.equal(event.owner,seller, 'owner is correct')
+         assert.equal(event.purchased, false, 'purchased is correct')
 
-        it('has a name', async () => {
-            const name = await marketplace.name()
-            assert.equal(name, "Grace Marketplace")
-        })
-
-    })
-})
+         await await marketplace.createProduct('', web3.utils.toWei('1', 'Ether'), { from: seller }).should.be.rejected;
+         await await marketplace.createProduct('', web3.utils.toWei('Think Deeper', 0), { from: seller }).should.be.rejected;
+     })
+  });
+});
