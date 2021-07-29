c1750025a8442803ffbce6e1406b7e4d422db63f
Grace Mugoiri <gracemugoiri@gmail.com>
2021-07-29 16:53:05 +0300

    add struct and store it in a mapping

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -2,8 +2,18 @@ pragma solidity ^0.5.0;
 
 contract Marketplace {
     string public name;
+    mapping(uint => Product) public products;
+    uint public productCount = 0;
 
     constructor() public {
         name = "Grace Marketplace";
+    }
+
+    struct Product {
+        uint id;
+        string name;
+        uint price;
+        address owner;
+        bool purchased;
     }
 }
