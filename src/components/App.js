771b3aced547dc48a0b178e19c9f85e8f4276d0d
Grace Mugoiri <gracemugoiri@gmail.com>
2021-08-02 20:31:01 +0300

    productCount function and bind it to

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import Web3 from 'web3';
 import './App.css';
 import Navbar from './Navbar';
 import Marketplace from '../abis/Marketplace.json';
+import Main from './Main';
+// import { load } from 'babel-register/lib/cache';
 
 class App extends Component {
   async componentWillMount() {
@@ -34,11 +36,25 @@ class App extends Component {
       const marketplace = web3.eth.Contract(Marketplace.abi, networkData.address)
       this.setState({ marketplace })
       const productCount = await marketplace.methods.productCount().call()
-      console.log(productCount.toString())
       this.setState({ loading: false })
+      for (var i = 1; i <= productCount; i++) {
+        const product = await marketplace.methods.products(i).call();
+        this.setState({ products: [...this.state.products, product]})
+      } 
     } else {
       window.alert('Marketplace contract not deployed to deteted network')
     }
+  }      
+
+  purchaseProduct(id, price) {
+    this.setState({ loading: true })
+    this.state.marketplace.methods.purchaseProduct(id).send({
+      from: this.state.account,
+      value: price
+    })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
   }
 
   createProduct(name, price) {
@@ -60,12 +76,23 @@ class App extends Component {
       loading: true
     }
     this.createProduct = this.createProduct.bind(this)
+    this.purchaseProduct = this.purchaseProduct.bind(this)
   }
 
   render() {
     return (
       <div>
         <Navbar account={this.state.account} />
+        <main role="main" className="col-lg-12 d-flex">
+          { this.state.loading
+            ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
+            : <Main
+                products={this.state.products}
+                createProduct={this.createProduct}
+                purchaseProduct={this.purchaseProduct}
+              />
+          }
+        </main>
       </div>
     )
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 
-
 class Navbar extends Component {
     render() {
         return (
